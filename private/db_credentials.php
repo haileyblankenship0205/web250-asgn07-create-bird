f2285911cb4b036cba6dbb8ed0b184a9b4c0ba20
Hailey Blankenship <you@example.com>
2020-10-27 18:23:34 -0400

    commit

---
+++ private/db_credentials.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file has login credentials for your localhost (on your home computer)
+ * 
+ * Fill in the credentials for your production (Where you host your files onine)
+ * 
+ * Comment out the Development code before uploading files to your Webhost
+ */
+
+// Development - Local
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "web250user");
+// define("DB_PASS", "8N=YwFDn4]");
+// define("DB_NAME", "wnc_birds");
+
+
+// Production - SiteGround
+define("DB_SERVER", "localhost");
+define("DB_USER", "haileyb5_user");
+define("DB_PASS", "BearDog98");
+define("DB_NAME", "haileyb5_wnc_birds");
